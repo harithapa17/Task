0134fab7e7af3385acb44c03496344b9812be03a
px-seema <amanjot.pixlerlab@gmail.com>
2022-07-27 17:40:19 +0530

    crud opertion in php.

--- crud/php/config.php
+++ crud/php/config.php
@@ -4,6 +4,6 @@ $link=mysqli_connect("localhost","root","","mydb");
 
 if($link == false){
   
-    echo " connection error." .mysqli_connect_error();
+    echo " connection h error." .mysqli_connect_error();
 
 }?> 
