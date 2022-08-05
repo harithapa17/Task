a0aff3c50bc79482e6e1a9113f45228b44755520
px-seema <amanjot.pixlerlab@gmail.com>
2022-08-05 18:01:23 +0530

    created a post upload table having the function to gave title and description and an action field having edit and delete option

---
+++ crud/post/controllers/PostController.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $data['posts'] = Post::orderBy('id','desc')->paginate(5);
+    
+        return view('index', $data);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required',
+            'image' => 'required|image|mimes:jpg,png,jpeg,gif,svg|max:2048',
+            'description' => 'required',
+        ]);
+        $path = $request->file('image')->store('public/images');
+        $post = new Post;
+        $post->title = $request->title;
+        $post->description = $request->description;
+        $post->image = $path;
+        $post->save();
+        session()->flash('success', 'Post has been created Successfully !');
+        return redirect()->route('index');
+        //return redirect()->route('index') ->with('success','Post has been created successfully.');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return view('create',compact('post'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        return view('edit',compact('post'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'title' => 'required',
+            'description' => 'required',
+        ]);
+        
+        $post = Post::find($id);
+        if($request->hasFile('image')){
+            $request->validate([
+              'image' => 'required|image|mimes:jpg,png,jpeg,gif,svg|max:2048',
+            ]);
+            $path = $request->file('image')->store('public/images');
+            $post->image = $path;
+        }
+        $post->title = $request->title;
+        $post->description = $request->description;
+        $post->save();
+    
+        return redirect()->route('index')
+                        ->with('success','Post updated successfully');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $post->delete();
+    
+        return redirect()->route('index') ->with('success','Post has been deleted successfully');
+    }
+}
